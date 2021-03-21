96be58dbe08c43d7a59b103493c5b81266aeb769
jesusaguilera <jesusaguilera09@gmail.com>
2021-03-21 17:42:17 +0100

    bend effect

--- src/assets/shaders/vertex.js
+++ src/assets/shaders/vertex.js
@@ -2,6 +2,8 @@ export default `
 
   varying vec2 vUv;
   uniform float u_time;
+  uniform float u_bend;
+  float PI = 3.14159265359;
 
   void main(){
 
@@ -14,6 +16,10 @@ export default `
     vUv.y += sin(u_time) * 0.02;
 
     vec3 pos = position;
+
+    // Bend  wireframe
+    pos.y += (sin(PI * uv.x) * u_bend * 0.5);
+    // pos.y = pos.y + ((sin(uv.x * PI) * u_bend * 24.0) * 0.125);
 
     // Oscillating movement on y axis 
     pos.y += sin(u_time) * 1.5;
--- src/components/Planes.js
+++ src/components/Planes.js
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect, useMemo } from "react"
 import * as THREE from "three";
 import { useFrame, useLoader } from "react-three-fiber";
 import { Plane } from "@react-three/drei";
-import lerp from "lerp";
+// import lerp from "lerp";
 
 // Shaders
 import fragment from '../assets/shaders/fragment';
@@ -11,6 +11,11 @@ import vertex from '../assets/shaders/vertex';
 // Images
 import img_01 from '../assets/images/01_img.jpg';
 
+let last = 0;
+
+const lerp = (a, b, t) => {
+  return ((1 - t) * a + t * b);
+}
 
 const Planes = (props) => {
 
@@ -18,20 +23,28 @@ const Planes = (props) => {
   const meshRef = useRef(null)
 
   const [aspect] = useState(() => new THREE.Vector2(150, 100))
+  const [wheelDeltaY, setWheelDeltaY] = useState(0)
+
+  useEffect(()=>{
+    meshRef.current.position.x = 75;
+  },[])
 
   const uniforms = useMemo(() => ({
     u_time: { type: "f", value: 0.0 },
-    u_tex: { type: "t", value: handImg }
+    u_tex: { type: "t", value: handImg },
+    u_bend: { type: "f", value: 0.0 }
   }),[])
 
 
   useFrame((state, delta) => {
+    window.addEventListener("wheel", (e) => setWheelDeltaY(e.deltaY))
     uniforms.u_time.value += delta;
-    meshRef.current.position.y = lerp(meshRef.current.position.y, props.smooth, 0.02);;
+    uniforms.u_bend.value = lerp(uniforms.u_bend.value, window.scrollY - meshRef.current.position.y, 0.14);
+    meshRef.current.position.y = lerp(meshRef.current.position.y, window.scrollY , 0.13);
   });
 
   return (
-    <Plane  ref={meshRef} onClick={() => alert("yes!")} args={[aspect.x, aspect.y, 20]}>
+    <Plane ref={meshRef} onClick={() => alert("yes!")} args={[aspect.x, aspect.y, 20]}>
       <shaderMaterial
         side={THREE.DoubleSide}
         uniforms={uniforms}
